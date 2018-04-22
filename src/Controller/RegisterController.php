72519e8478bc1b8c6f3ecea6b8daf640452c2ea1
Matias Villarroel <m.villarroel.2015@salleurl.edu>
2018-04-22 23:17:50 +0200

    Subir foto de perfil

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -51,7 +51,8 @@ class RegisterController
         $description = $resul['description'];
         $name = $resul['name'];
         $characteristics = $resul['characteristics'];
-        $foto = $resul['picture'];
+        //$foto = $resul['picture'];
+        $foto = "/path";
 
 
         if (filter_var($email, FILTER_VALIDATE_EMAIL) && strlen($password) >= 6 && strlen($password)<= 12
@@ -64,6 +65,7 @@ class RegisterController
                 $exit = $this->container->get('user_repository')->save($user);
                 if($exit) {
                     shell_exec("mkdir /home/vagrant/users/$email");
+                    $this->uploadImage();
                     return $this->container->get('view')->render($response, 'login.twig', ['email' => $email]);
                 } else {
                     var_dump($resul);
@@ -79,6 +81,49 @@ class RegisterController
         } else {
             echo "ERROR";
             //TODO ha habido un error en la creacion del usuario
+        }
+
+    }
+
+    private function uploadImage(){
+        var_dump($_FILES);
+        $target_dir = "/home/vagrant/profilePics/";
+        $target_file = $target_dir . basename($_FILES["picture"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        // Check if image file is a actual image or fake image
+        if(isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["picture"]["tmp_name"]);
+            if($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+        }
+
+        if (file_exists($target_file)) {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+        }
+
+        // Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif" ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+            // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($_FILES["picture"]["tmp_name"], $target_file)) {
+                echo "The file ". basename( $_FILES["picture"]["name"]). " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
         }
 
     }
