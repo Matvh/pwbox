607544cd83c27326eadb80a71fe34be9c73e67a4
Rada <jesus.rada.2015@salleurl.edu>
2018-05-03 18:28:12 +0200

    Foto imagen + JS login

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,18 +1,49 @@
-$(function() {
+var PASS_REG = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,12}$/;
 
-    $('#login-form-link').click(function(e) {
-		$("#login-form").delay(100).fadeIn(100);
- 		$("#register-form").fadeOut(100);
-		$('#register-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-	$('#register-form-link').click(function(e) {
-		$("#register-form").delay(100).fadeIn(100);
- 		$("#login-form").fadeOut(100);
-		$('#login-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
 
-});
+var CORRECT = 'rgb(104, 134, 121)';
+var INCORRECT = 'rgb(224, 155, 93)'
+
+var user = false;
+var pass = false;
+
+function checkError(e, t) {
+
+	switch (t){
+		case 'text':
+			if(e.value == 'a'){
+                e.style.setProperty('background-color', INCORRECT, 'important');
+                user = false;
+                blockSubmit();
+            }
+			else{
+                e.style.setProperty('background-color', CORRECT, 'important');
+                user = true;
+                if(pass) allowSubmit();
+			}
+			break;
+
+		case 'pass':
+
+			if(!PASS_REG.test(e.value)){
+                e.style.setProperty('background-color', INCORRECT, 'important');
+                pass = false;
+                blockSubmit();
+            }
+            else{
+                e.style.setProperty('background-color', CORRECT, 'important');
+                pass = true;
+                if(user) allowSubmit();
+            }
+            break;
+	}
+
+}
+
+function allowSubmit(){}
+
+function blockSubmit(){}
+
+function preventDefault(){
+    return pass && user;
+}
