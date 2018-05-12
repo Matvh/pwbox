db5fd2ebb93523c33abf472e8d6be82db7da5b0e
Rada <jesus.rada.2015@salleurl.edu>
2018-05-12 16:30:05 +0200

    Intentando subir archivos

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -29,7 +29,7 @@ function openFileDialog() {
     $.FileDialog({
 
         // MIME type of accepted files, e. g. image/jpeg
-        accept: "*",
+        accept: "png, md, pdf, txt, gif",
         cancelButton: "Close",
         dragMessage: "Drop files here",
         dropheight: 400,
@@ -42,16 +42,40 @@ function openFileDialog() {
         removeMessage: "Remove&nbsp;file",
         title: "Upload in current folder"
     });
-}
 
-// handle files choice when done
-on('files.bs.filedialog', function(ev) {
-    var files_list = ev.files;
-    // DO SOMETHING
-});
+    // handle files choice when done
+    on('files.bs.filedialog', function(ev) {
+        var files_list = ev.files;
+
+        var message;
+
+        var formData = new FormData();
+        formData.append('files', files_list);
+
+        $.ajax({
+            url: "/file",
+            type: "post",
+            contentType: false,
+            processData: false,
+            data: formData,
+            async: false,
+
+            success: function (msg) {
+                console.log(msg);
+            },
+
+            error: function (msg) {
+                console.log(msg);
+            },
+            cache: false
+        });
+        // DO SOMETHING
+    });
 
 
 // handle dialog cancelling
-on('cancel.bs.filedialog', function(ev) {
-    // DO SOMETHING
-});
+    on('cancel.bs.filedialog', function(ev) {
+        // DO SOMETHING
+    });
+}
+
